578a6ee11bcbc1096629ed118ca7eae264d775d7
ming0614 <3075624299@qq.com>
2021-01-05 11:48:53 +0800

    mail

---
+++ node/cherrio/mail/index.js
@@ -0,0 +1,55 @@
+// require 关键字， 引入库
+// const fs = require('fs');//文件系统 内置模块
+const nodemailer = require('nodemailer'); // 第三方库， 安装的
+const {default:Axios}=require('axios');
+const schedule  = require('node-schedule');
+// 模块 语法 -> 函数模块化  -> module (水电工、木工、..)
+// 现代语言中， 一个文件就是一个模块 ....
+// const modA = require('./a.js'); // 相对地址
+// console.log(modA.add(1,2));
+// fs.readFile('./readme.md', function(err, res) {
+//   if (err) {
+//     throw new Error(err);
+//   }
+//   console.log(res.toString());
+// })
+// async + await 
+// 异步函数 
+// 把土味的情话 
+function getHoneyedWords() {
+    const url='https://chp.shadiao.app/api.php';
+    return Axios.get(url);
+}
+// 把发邮件这件事， 封装给一个函数
+async function sendMail(words) {
+  let user = "3075624299@qq.com"
+  let pass = "tgkrxbkulazvddce"; //  授权码
+  let to = "1471867575@qq.com";
+  let transporter = nodemailer.createTransport({
+    host: "smtp.qq.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: user,
+      pass: pass
+    }
+  });
+  let info = await transporter.sendMail({
+    from: `爸爸<${user}>`, // sender address
+    to: `儿子<${to}>`,
+    subject: "给儿子的一封信",
+    text: words
+  })
+  console.log("发送成功");
+}
+
+// sendMail();
+schedule.scheduleJob({hour: 12, minute: 03}, function() {
+    console.log("启动任务：" + new Date());
+    getHoneyedWords()
+    .then(res => {
+      sendMail(res.data);
+      // console.log();
+    })
+  })
+  
---
+++ node/cherrio/mail/readme.md
@@ -0,0 +1,10 @@
+- 队长每天被辅导员烦死+
+    发条信息
+    npm i -g stylus global 全局
+    npm i nodemailer 当前项目
+
+- JS require + module.exports common.js 模块化
+    1. 内置模块
+    2. 自己封装模块 一个文件就是一个模块 一个文件夹就是一个模块组
+        原始社会 -> 封建社会(module+require) -> 资本主义社会(docker =k8s 省钱， 快速运维) -> 共产主义(资本人)
+    3. nodemailer 常用的邮件模块
