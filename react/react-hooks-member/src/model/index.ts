383efbc6e9b2731dd2e07cb6cc75018ce65470b8
ming0614 <3075624299@qq.com>
2021-03-08 21:43:57 +0800

    members

---
+++ react/react-hooks-member/src/model/index.ts
@@ -0,0 +1,3 @@
+// 企业级开发中  每个目录都加一个index.js
+// 帮助管理目录下的所有模块
+export * from './memberEntity';
---
+++ react/react-hooks-member/src/model/memberEntity.ts
@@ -0,0 +1,7 @@
+// 数据校验 ts的独有的  数据库里的
+// menber type 
+export interface MemberEntity {
+    id: number;
+    login: string;
+    avatar_url: string;
+}
