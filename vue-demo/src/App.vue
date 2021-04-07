051c3a11ec197f3c82bb3e56f25a8e7e31383599
ming0614 <3075624299@qq.com>
2021-04-07 21:54:22 +0800

    vue

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <todolist />
+  <!-- <list /> -->
+  
+</template>
+
+<script>
+
+import todolist from './components/todolist'; 
+export default {
+  name: 'App',
+  components: {
+    todolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-demo/src/components/todolist.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Vue3 todos</h1>
+      <input type="text" class="new-todo" placeholder="想干的事" @keyup.enter="addTodo" v-model="newTodo">
+    </header>
+  </section>
+  <section class="main">
+    <ul class="todo-list">
+      <li v-for="(todo,index) in todos" :key="todo.id"  class="todo">
+          <div class="view">
+              <label>{{todo.title}}</label>
+              <button class="destroy" @click="removeTodo(index)"></button>
+          </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+export default {
+  setup () {
+    const state = reactive({
+        newTodo:'',
+      todos: [
+        { id: '1', title: '吃饭', complated: false },
+        { id: '2', title: '睡觉', complated: false }
+      ]
+    })
+    function addTodo(){
+        //console.log(state.newTodo);
+        let value =state.newTodo && state.newTodo.trim()
+        if(!value) return 
+        state.todos.push({
+            id:state.todos.length+1,
+            title:value,
+            complated:false
+        })
+        state.newTodo=''
+    }
+    function removeTodo(index){
+        console.log(index)
+        state.todos.splice(index,1)
+    }
+    return {
+        ...toRefs(state),
+        addTodo,
+        removeTodo
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
