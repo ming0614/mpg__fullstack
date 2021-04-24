d67e0c0bf868f2f52dcf6c2cb1038baeb1c0ceca
ming0614 <3075624299@qq.com>
2021-04-24 16:54:12 +0800

    js执行机制

---
+++ js执行机制/index.js
@@ -0,0 +1,9 @@
+// showName()
+// console.log(myname)
+var myname = undefined
+function showName(){
+    console.log('mpg11111showname执行')
+}
+showName()
+console.log(myname)
+myname= 'mpg'
---
+++ js执行机制/闭包/index.js
@@ -0,0 +1,36 @@
+// function bar(){
+//     console.log(myName)
+// }
+// function foo(){
+//     var myName = 'jx'
+//     bar()
+// }
+// var myName = 'wjx'
+// foo()
+
+
+// let 
+function bar() {
+    var myName = '嘉新'
+    let test1 = 100
+    if (1) {
+      let myName = 'chrome浏览器'
+      console.log(test);
+    } 
+  }
+  
+  function foo() {
+    var myName = '王嘉新'
+    let test = 2
+    {
+      let test = 3
+      bar()
+    }
+  }
+  
+  var myName = '队长'
+  let myAge = 10
+  let test = 1
+  foo()
+  
+  
